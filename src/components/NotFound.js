22e603edda5af8f5cc850cec675fe5fea13875c3
Kim Yuen <kim_man@live.com>
2021-01-08 18:12:01 +1100

    first commit - having issues with this.props.history.push(`/store/${storeName}`);

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFound = () => (
+  <div>
+    <h2>Not foundddd</h2>
+  </div>
+);
+
+export default NotFound;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import "./css/style.css";
+import Router from "./components/Router";
+import StorePicker from "./components/StorePicker";
+render(<Router />, document.querySelector("#main"));
